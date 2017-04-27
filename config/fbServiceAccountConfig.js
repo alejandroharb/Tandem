172e9f98970d419424e5ddcfce3cab3ae52a3955
alejandro harb <alejandro harb>
2017-04-27 12:26:45 -0500

    removed gitignore

---
+++ config/fbServiceAccountConfig.js
@@ -0,0 +1,3 @@
+var serviceAccount = require("./tandem-f181c-firebase-adminsdk-bhbky-f8976ae9a3.json"); //currently with firebase for Grow-with-Friends until migration
+
+module.exports = serviceAccount;
