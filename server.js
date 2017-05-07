84aec5f96a66665f2f4c777749b9adb75406740a
alejandro harb <alejandro harb>
2017-05-07 18:53:20 -0500

    turned off sequelize force table drops

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ for (let route in routes){
   app.use(route, routes[route]);
 }
 
-db.sequelize.sync({force:true}).then(function () {
+db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("listening on Port: " + PORT);
     })
