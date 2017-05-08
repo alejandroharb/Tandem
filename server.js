9d1451e2abbfb12e8e1ec22e4cd8b4a3f242ba48
alejandro harb <alejandro harb>
2017-05-07 19:15:08 -0500

    debugged express.static

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
     secret: "boiling kettle",
     resave: false,
